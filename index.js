afe618f1431313c33dec6c022fa2ab374d83d680
erowii <erowii0629@gmail.com>
2020-09-19 16:44:31 +0800

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//https://www.npmjs.com/package/linebot
+//API https://www.npmjs.com/package/linebot
 const linebot = require('linebot');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars，可參考 Step2
 const bot = linebot({
@@ -8,19 +10,39 @@ const bot = linebot({
     channelAccessToken: process.env.channelAccessToken
 });
 
+const app = express();
+
+const parser = bodyParser.json({
+    verify: function(req, res, buf, encoding) {
+        req.rawBody = buf.toString(encoding);
+    }
+});
+
+app.post('/linewebhook', parser, function(req, res) {
+    if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
+        return res.sendStatus(400);
+    }
+    bot.parse(req.body);
+    return res.json({});
+});
+
 // 當有人傳送訊息給 Bot 時
 bot.on('message', function(event) {
     // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
     event.reply(`你說了 ${event.message.text}`);
 });
 
-// Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
-bot.listen('/linewebhook', process.env.PORT || 5000, function() {
-    console.log('全國首家LINE線上機器人上線啦！！');
+app.listen(process.env.PORT || 5000, function() {
+    console.log('LineBot is running.');
 });
 
+// Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
+// bot.listen('/linewebhook', process.env.PORT || 5000, function() {
+//     console.log('全國首家LINE線上機器人上線啦！！');
+// });
+
 //line啟動時發送訊息
-bot.broadcast({
-    "type": "text",
-    "text": "LineBot Action!"
-});
+// bot.broadcast({
+//     "type": "text",
+//     "text": "LineBot Action!"
+// });
