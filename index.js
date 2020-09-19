dd9dd6f94fd1818781ff1d0b2825cdc8836ea339
erowii <erowii0629@gmail.com>
2020-09-19 15:34:55 +0800

    commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const http = require("http");
+const linebot = require("linebot");
 
 // const hostname = "127.0.0.1";
 const port = process.env.PORT || 5000;
@@ -21,3 +22,19 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port);
+
+var bot = linebot({
+	channelId: '1654940136',
+	channelSecret: '8a4dcd0f2983e9b2e9d2b9db43517803',
+	channelAccessToken: 'QbmZPCahmyMALl+M34eFPfiPtYrFtmFtC6K9SRM7ZhnWdAJGB/50V5YYZLuUCkFvXnFk29r5yHqQdCQnfe0IoYKQLekJ9muHefED4CI6ohfdNcrMioLPwpSdtYOJRbXHuPJtfE7/I/P6wtvSNm3ypQdB04t89/1O/w1cDnyilFU='
+});
+
+bot.on('message', function (event) {
+  // event.message.text是使用者傳給bot的訊息
+  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
+  event.reply(event.message.text).then(function (data) {
+    // 當訊息成功回傳後的處理
+  }).catch(function (error) {
+    // 當訊息回傳失敗後的處理
+  });
+});
